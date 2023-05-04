a61a9b5b9fd92e186af6973203afdc1e7baddd11
Jon Ellwood <jonellwood@gmail.com>
2023-05-04 13:03:15 -0400

    props completed

---
+++ firstsfc/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>Food</h1>
+  <div id="wrapper">
+    <food-item food-name="Apples" food-desc="Stupid red things" v-bind:is-favorite="false" />
+    <food-item
+      food-name="Vegan Pizza"
+      food-desc="Plants plants and more yummy plants"
+      v-bind:is-favorite="true"
+    />
+    <food-item
+      food-name="Rice"
+      food-desc="Grain people eat when the need to feel full"
+      v-bind:is-favorite="false"
+    />
+  </div>
+</template>
+
+<script></script>
+<style>
+#wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+#wrapper > div {
+  border: dashed 1px black;
+  flex-basis: 120px;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightgreen;
+}
+</style>
---
+++ firstsfc/src/components/FoodItem.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>
+    <h2>
+      {{ foodName }}
+      <!-- <img src="/public/img_quality.svg" v-show="isFavorite" /> -->
+      <img src="/img_quality.svg" v-show="foodIsFavorite" />
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
+  </div>
+</template>
+
+<!-- <script>
+export default {
+  props: ['foodName', 'foodDesc', 'isFavorite'],
+  //   props: {
+  //     foodName: {
+  //       type: String,
+  //       required: true
+  //     },
+  //     foodDesc: {
+  //       type: String,
+  //       required: false,
+  //       default: 'This is generic description of the thing you are reading about right now',
+  //       //   we are adding a valdator function to make sure the length is withing the right range
+  //       validator: function (value) {
+  //         if (20 < value.length && value.length < 50) {
+  //           return true
+  //         } else {
+  //           return false
+  //         }
+  //       }
+  //     },
+  //     isFavorite: Boolean
+  //   },
+  data() {
+    return {
+      foodIsFavorite: this.isFavorite
+    }
+  },
+  methods: {
+    toggleFavorite() {
+      this.foodIsFavorite = !this.foodIsFavorite
+    }
+  }
+}
+</script> -->
+<script>
+export default {
+  props: ['foodName', 'foodDesc', 'isFavorite'],
+  data() {
+    return {
+      foodIsFavorite: this.isFavorite
+    }
+  },
+  methods: {
+    toggleFavorite() {
+      this.foodIsFavorite = !this.foodIsFavorite
+    }
+  }
+}
+</script>
+
+<style>
+img {
+  height: 1.5em;
+  float: right;
+}
+</style>
