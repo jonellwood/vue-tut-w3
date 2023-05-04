e6e89383958c032b312e7516c4f53084898310df
Jon Ellwood <jonellwood@gmail.com>
2023-05-04 13:15:27 -0400

    v-for Components complete

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,21 +1,41 @@
 <template>
   <h1>Food</h1>
+  <button @click="removeItem">Remove Item</button>
   <div id="wrapper">
-    <food-item food-name="Apples" food-desc="Stupid red things" v-bind:is-favorite="false" />
     <food-item
-      food-name="Vegan Pizza"
-      food-desc="Plants plants and more yummy plants"
-      v-bind:is-favorite="true"
-    />
-    <food-item
-      food-name="Rice"
-      food-desc="Grain people eat when the need to feel full"
-      v-bind:is-favorite="false"
+      v-for="x in foods"
+      :key="x.name"
+      :food-name="x.name"
+      :food-desc="x.desc"
+      :is-favorite="x.favorite"
     />
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      foods: [
+        { name: 'Apples', desc: 'Apples are a type of fruit that grow on trees.', favorite: true },
+        {
+          name: 'Pizza',
+          desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
+          favorite: false
+        },
+        { name: 'Rice', desc: 'Rice is a type of grain that people like to eat.', favorite: false },
+        { name: 'Fish', desc: 'Fish is an animal that lives in water.', favorite: true },
+        { name: 'Cake', desc: 'Cake is something sweet that tastes good.', favorite: false }
+      ]
+    }
+  },
+  methods: {
+    removeItem() {
+      this.foods.splice(1, 1)
+    }
+  }
+}
+</script>
 <style>
 #wrapper {
   display: flex;
